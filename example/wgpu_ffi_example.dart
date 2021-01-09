03352b3ea42805d95c5acf71d34a970d41f3ae3b
J-P Nurmi <jpnurmi@gmail.com>
2021-01-09 17:08:40 +0100

    Import Dart package template

---
+++ example/wgpu_ffi_example.dart
@@ -0,0 +1,6 @@
+import 'package:wgpu_ffi/wgpu_ffi.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/wgpu_ffi.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library wgpu_ffi;
+
+export 'src/wgpu_ffi_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
