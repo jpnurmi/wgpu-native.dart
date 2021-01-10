d75806b77b4e8b4b63202d91aadc37fdfda39f2c
J-P Nurmi <jpnurmi@gmail.com>
2021-01-10 12:36:02 +0100

    Fix resolving of dylib location

--- lib/src/dylib.dart
+++ lib/src/dylib.dart
@@ -1,9 +1,18 @@
 import 'dart:ffi' as ffi;
+import 'dart:io';
 
 import 'package:dylib/dylib.dart';
 
 import 'bindings.dart';
 
 LibWGPU? _dylib;
-LibWGPU get dylib => _dylib ??= LibWGPU(
-    ffi.DynamicLibrary.open(resolveDylibPath('wgpu_native', 'LIBWGPU_PATH')));
+LibWGPU get dylib {
+  return _dylib ??= LibWGPU(ffi.DynamicLibrary.open(
+    resolveDylibPath(
+      'wgpu_native',
+      path: Directory.current.path,
+      dartDefine: 'libwgpu-path',
+      environmentVariable: 'LIBWGPU_PATH',
+    ),
+  ));
+}
